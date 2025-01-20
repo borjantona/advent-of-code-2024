847d6a88f0c9a1c5f9e4ae41de677dc8bc51a56c
Borjuto <borja.antona@gmail.com>
2025-01-20 12:26:23 +0100

    feat: solve day 5 part 1

---
+++ src/day-05/day-05-part1.js
@@ -0,0 +1,51 @@
+import { getInputLists } from "../utils/utils.js";
+
+let pageRules = [];
+let pagesToProduce = [];
+
+function isValidBefore(value, compareValue) { // 75 47
+  return !pageRules.includes(`${compareValue}|${value}`);
+}
+function isValidAfter(value, compareValue) { // 47 75
+  return !pageRules.includes(`${value}|${compareValue}`);
+}
+function findMiddleValue(array) {
+  //console.log(array);
+  return +array[Math.floor(array.length / 2)];
+}
+
+function main() {
+  const file = getInputLists("./src/inputs-and-docs/day05/input.txt");
+  let sumMiddles = 0;
+  pageRules = file.filter((val) => val.includes("|"));
+  pagesToProduce = file.filter((val) => val.includes(","));
+
+  for (let pages of pagesToProduce) {
+    const pagesArray = pages.split(",");
+	let validPages = true;
+
+    for (let i = 0; i < pagesArray.length - 1; i++) {
+      const page = pagesArray[i];
+      
+      /* CHECKS BEFORE */
+      for (let j = i + 1; j < pagesArray.length; j++) {
+        const pageToCompare = pagesArray[j];
+        //console.log(`Is ${page} before ${pageToCompare}?: ${isValidBefore(page, pageToCompare)}`)
+        //console.log(isValidBefore(page, pageToCompare));
+        validPages &&= isValidBefore(page, pageToCompare);
+      }
+      /* CHECKS AFTER */
+      for (let z = i - 1; z >= 0; z--) {
+        const pageToCompare = pagesArray[z];
+        //console.log(`Is ${page} after ${pageToCompare}?: ${isValidAfter(pageToCompare ,page)}`)
+        validPages &&= isValidAfter(page, pageToCompare);
+      }
+    }
+    if (!validPages) continue;
+    sumMiddles += findMiddleValue(pagesArray);
+  }
+
+  console.log(sumMiddles);
+}
+
+main();
