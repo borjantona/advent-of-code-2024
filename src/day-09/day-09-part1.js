199cf6f65f58b02e5058eb751f29c6cd1bf45271
Borjuto <borja.antona@gmail.com>
2025-02-04 17:46:23 +0100

    feat: solve day 9 part 2

--- src/day-09/day-09-part1.js
+++ src/day-09/day-09-part1.js
@@ -1,54 +1,56 @@
 import { getFile } from "../utils/utils.js";
 
 function arrangeFiles(disk) {
-	let resultDisk = [];
-	let emptySpace = false;
-	let index = 0;
-	for (let i = 0; i < disk.length; i++) {
-		const diskMapVar = parseInt(disk[i]);
-		const newArr = (emptySpace) ? Array(diskMapVar).fill('.') : Array(diskMapVar).fill(index)
-		resultDisk = [...resultDisk, ...newArr];
-		if (emptySpace) index++;
-		emptySpace = !emptySpace;
-	}
-	//console.log(resultDisk);
-	return resultDisk;
+  let resultDisk = [];
+  let emptySpace = false;
+  let index = 0;
+  for (let i = 0; i < disk.length; i++) {
+    const diskMapVar = parseInt(disk[i]);
+    const newArr = emptySpace
+      ? Array(diskMapVar).fill(".")
+      : Array(diskMapVar).fill(index);
+    resultDisk = [...resultDisk, ...newArr];
+    if (emptySpace) index++;
+    emptySpace = !emptySpace;
+  }
+  //console.log(resultDisk);
+  return resultDisk;
 }
 
 function fragmentDisk(disk) {
-	const newDisk = [...disk];
-	for(let i = newDisk.length - 1; i >= 0; i--) {
-		const diskMapVar = newDisk[i];
-		//console.log(`DiskMapVar ${diskMapVar} Index ${i}`)
-		if (diskMapVar === '.') continue;
-		const firstEmpty = newDisk.findIndex((val) => val === '.');
-		//console.log(`firstEmpty ${firstEmpty}`)
-		//console.log(newDisk.join(''))
-		if (firstEmpty > i) break;
-		if (firstEmpty !== -1) {
-			newDisk[firstEmpty] = diskMapVar;
-			newDisk[i] = '.';
-		}
-	}
-	return newDisk;
+  const newDisk = [...disk];
+  for (let i = newDisk.length - 1; i >= 0; i--) {
+    const diskMapVar = newDisk[i];
+    //console.log(`DiskMapVar ${diskMapVar} Index ${i}`)
+    if (diskMapVar === ".") continue;
+    const firstEmpty = newDisk.findIndex((val) => val === ".");
+    //console.log(`firstEmpty ${firstEmpty}`)
+    //console.log(newDisk.join(''))
+    if (firstEmpty > i) break;
+    if (firstEmpty !== -1) {
+      newDisk[firstEmpty] = diskMapVar;
+      newDisk[i] = ".";
+    }
+  }
+  return newDisk;
 }
 
 function checksum(disk) {
-	let value = 0;
-	let index = 0;
-	for (const val of disk) {
-		if (val === '.') break;
-		value += val * index;
-		index++;
-	}
-	return value;
+  let value = 0;
+  let index = -1;
+  for (const val of disk) {
+    index++;
+    if (val === ".") continue;
+    value += val * index;
+  }
+  return value;
 }
 
-function main () {
-	const disk = getFile('./src/inputs-and-docs/day-09/input.txt');
-	const diskArranged = arrangeFiles(disk);
-	const diskFragmented = fragmentDisk(diskArranged);
-	console.log(checksum(diskFragmented));
+function main() {
+  const disk = getFile("./src/inputs-and-docs/day-09/input.txt");
+  const diskArranged = arrangeFiles(disk);
+  const diskFragmented = fragmentDisk(diskArranged);
+  console.log(checksum(diskFragmented));
 }
 
 main();
---
+++ src/day-09/day-09-part2.js
@@ -0,0 +1,96 @@
+import { getFile } from "../utils/utils.js";
+
+function arrangeFiles(disk) {
+  let resultDisk = [];
+  let emptySpace = false;
+  let index = 0;
+
+  for (let i = 0; i < disk.length; i++) {
+    const diskMapVar = parseInt(disk[i]);
+    const newArr = new Array(diskMapVar).fill(emptySpace ? "." : index);
+    resultDisk.push(...newArr);
+    if (emptySpace) index++;
+    emptySpace = !emptySpace;
+  }
+  return resultDisk;
+}
+
+function fragmentDisk(disk) {
+  let newDisk = disk.slice(); // Copia directa para evitar modificar el original
+  const diskLength = newDisk.length;
+
+  for (let i = diskLength - 1; i >= 0; i--) {
+    const percent = ((100 * (diskLength - i)) / diskLength).toFixed(2);
+    if (["25.00", "50.00", "75.00", "95.00"].includes(percent)) {
+      console.log(`Fragmenting... ${percent}%`);
+    }
+
+    const diskMapVar = newDisk[i];
+    if (diskMapVar === ".") continue;
+
+    const fileSize = getFileSize(diskMapVar, i, newDisk);
+    const firstEmpty = newDisk.indexOf(".");
+
+    if (firstEmpty !== -1) {
+      const indexAvailable = isSpaceAvailable(firstEmpty, newDisk, fileSize, i);
+      if (indexAvailable !== -1) {
+        newDisk.fill(diskMapVar, indexAvailable, indexAvailable + fileSize);
+        setEmptySpace(diskMapVar, newDisk, indexAvailable + fileSize);
+      }
+    }
+  }
+  return newDisk;
+}
+
+function isSpaceAvailable(start, disk, fileSize, end) {
+  let indexAvailable = -1;
+  for (let i = start; i <= end; i++) {
+    if (disk[i] === ".") {
+      const emptySpace = getEmptySpace(i, disk);
+      if (emptySpace >= fileSize) {
+        indexAvailable = i;
+        break;
+      }
+    }
+  }
+  return indexAvailable;
+}
+
+function getFileSize(index, pos, disk) {
+  let count = 0;
+  let start = Math.max(0, pos - 10);
+  let end = Math.min(disk.length, pos + 10);
+
+  for (let i = start; i < end; i++) {
+    if (disk[i] === index) count++;
+  }
+  return count;
+}
+
+function getEmptySpace(index, disk) {
+  let count = 0;
+  for (let i = index; i < disk.length && disk[i] === "."; i++) {
+    count++;
+    if (count > 10) break; // Limitar para evitar recorridos largos
+  }
+  return count;
+}
+
+function setEmptySpace(index, disk, start) {
+  for (let i = start; i < disk.length; i++) {
+    if (disk[i] === index) disk[i] = ".";
+  }
+}
+
+function checksum(disk) {
+  return disk.reduce((acc, val, idx) => (val === "." ? acc : acc + val * idx), 0);
+}
+
+function main() {
+  const disk = getFile("./src/inputs-and-docs/day-09/input.txt");
+  const diskArranged = arrangeFiles(disk);
+  const diskFragmented = fragmentDisk(diskArranged);
+  console.log(checksum(diskFragmented));
+}
+
+main();
